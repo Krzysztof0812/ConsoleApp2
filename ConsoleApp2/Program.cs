279c45f3199f38c250f1eb5d58e1fa9d07460a14
Krzysztof0812 <kallpa@tlen.pl>
2024-04-08 23:49:04 +0200

    zmienne: name, gender, age

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+    
+        var firstname = "Ewa";
+        var gender= "Woman";        
+        var age = 30;
+
+{
+
+
+    if
+
+    (
+gender == "Woman" && firstname == "Ewa" && age == 30)
+
+    {
+        Console.WriteLine("Ewa 30 lat");
+    }
+
+    else if
+
+       (age < 30 && gender == "Woman")
+
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+
+    else if
+
+        (gender == "man" && age < 18)
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna");
+
+    }
+
+}
+
+
+                                                              
+                                  
+                                    
+                                    
+              
+
+// if age == 30 && Woman = true 
+
