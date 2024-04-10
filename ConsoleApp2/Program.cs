96ebe36c81ade9879265c41b44a9178136247ca7
Krzysztof0812 <kallpa@tlen.pl>
2024-04-10 10:57:57 +0200

    zmienne po korekcie

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,47 +1,30 @@
 ﻿
 
 
-    
-        var firstname = "Ewa";
-        var gender= "Woman";        
-        var age = 30;
 
-{
+using System;
+
+var firstname = "Ewa";
+var gender = "Woman";
+var age = 30;
 
 
-    if
-
-    (
-gender == "Woman" && firstname == "Ewa" && age == 30)
-
+    if (gender == "Woman" && firstname == "Ewa" && age == 30)
     {
         Console.WriteLine("Ewa 30 lat");
     }
-
-    else if
-
-       (age < 30 && gender == "Woman")
-
+    else if (age < 30 && gender == "Woman")
     {
         Console.WriteLine("Kobieta poniżej 30 lat");
     }
-
-    else if
-
-        (gender == "man" && age < 18)
+    else if (gender == "man" && age < 18)
     {
         Console.WriteLine("Niepełnoletni mężczyzna");
-
     }
 
-}
 
 
-                                                              
-                                  
-                                    
-                                    
-              
 
-// if age == 30 && Woman = true 
+
+
 
